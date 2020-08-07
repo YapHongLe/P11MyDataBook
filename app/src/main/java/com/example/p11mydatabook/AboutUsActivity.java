6e308131d03544e7f5852b8437d95e5ca7dcdfa7
Yap Hong Le <18045404@myrp.edu.sg>
2020-08-07 13:55:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p11mydatabook/AboutUsActivity.java
@@ -0,0 +1,27 @@
+package com.example.p11mydatabook;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
---
+++ app/src/main/java/com/example/p11mydatabook/BioFragment.java
@@ -0,0 +1,87 @@
+package com.example.p11mydatabook;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BioFragment extends Fragment {
+
+    public BioFragment() {
+        // Required empty public constructor
+    }
+
+    private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
+
+    TextView textView;
+    Button btnEdit;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_bio_fragment, container, false);
+
+        textView = view.findViewById(R.id.textview);
+        btnEdit = view.findViewById(R.id.btnEdit);
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                alertDialog.setTitle("Edit Bio");
+                final EditText input = new EditText(getActivity());
+
+                alertDialog.setView(input);
+
+                alertDialog.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                textView.setText(input.getText().toString());
+                            }
+                        });
+                alertDialog.setNegativeButton("CANCEL",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog.show();
+            }
+        });
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    drawerLayout.openDrawer(GravityCompat.START);
+                } else{
+                    drawerLayout.closeDrawer(GravityCompat.END);
+                }
+            }
+        });
+
+        return view;
+    }
+}
